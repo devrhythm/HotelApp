7f80917dbf2e0cc903ad7aa0f1191d0fc52c5073
SUMETH K <sumeth_dev@live.com>
2022-08-29 16:25:53 +0700

    add class ContactPerson

---
+++ HotelApp/Models/ContactPerson.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelApp.Models
+{
+  internal class ContactPerson : Guest
+  {
+    public ContactPerson(string name, int age, List<string> phoneNumbers) : base(name, age, phoneNumbers)
+    {
+      IsContactPerson = true;
+    }
+    public ContactPerson(string name, int age, string phone) : this(name, age, new List<string>() { name })
+    {
+
+    }
+  }
+}
