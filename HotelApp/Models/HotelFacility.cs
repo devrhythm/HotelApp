ae521d7fbc5a17870d749d34dbef1678a871b6f9
SUMETH K <sumeth_dev@live.com>
2022-08-29 16:47:49 +0700

    add class HotelFacility

---
+++ HotelApp/Models/HotelFacility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Models
+{
+  internal class HotelFacility
+  {
+    public Guid HotelId { get; }
+    public string FacilityName { get; }
+    public int FacilityAmount { get; }
+
+    public HotelFacility(Guid hotelId, string facilityName, int facilityAmount)
+    {
+      HotelId = hotelId;
+      FacilityName = facilityName;
+      FacilityAmount = facilityAmount;
+    }
+
+
+  }
+}
