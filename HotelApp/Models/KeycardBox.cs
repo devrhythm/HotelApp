511e05884999fa63f46f7366b2f2497acfb4277f
SUMETH K <sumeth_dev@live.com>
2022-08-29 18:42:38 +0700

    add class KeycardBox

---
+++ HotelApp/Models/KeycardBox.cs
@@ -0,0 +1,32 @@
+﻿namespace HotelApp.Models
+{
+  internal class KeycardBox
+  {
+    private static KeycardBox _instance = new KeycardBox();
+    public static KeycardBox Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = new KeycardBox();
+        }
+        return _instance;
+      }
+    }
+
+    public ICollection<Keycard> Keycards { get; private set; } = new SortedSet<Keycard>();
+
+    private KeycardBox()
+    {
+
+    }
+
+    public Keycard GetNewKeycard()
+    {
+      Keycard newKeycard = new Keycard();
+      Keycards.Add(newKeycard);
+      return newKeycard;
+    }
+  }
+}
