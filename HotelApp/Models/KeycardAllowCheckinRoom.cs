970457595f27fafee6bf74ace5c14bdef8caca34
SUMETH K <sumeth_dev@live.com>
2022-08-29 16:50:26 +0700

    Add class KeycardAllowCheckinRoom

---
+++ HotelApp/Models/KeycardAllowCheckinRoom.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelApp.Models
+{
+  internal class KeycardAllowCheckinRoom : Keycard
+  {
+    public Guid RoomId { get; }
+
+    public KeycardAllowCheckinRoom(Guid roomId) : base()
+    {
+      RoomId = roomId;
+    }
+  }
+}
