2be1fa9fb1074fcb18ae10faeeaaa050da09c4c2
SUMETH K <sumeth_dev@live.com>
2022-08-29 17:13:59 +0700

    Edit class RoomType : fix code convention

--- HotelApp/Models/RoomType.cs
+++ HotelApp/Models/RoomType.cs
@@ -4,8 +4,8 @@
   {
     public string Name { get; }
     public int MaxGuestAMount { get; }
-
     public ICollection<InRoomFacility> Commudities { get; private set; }
+
     public RoomType(string typeName, int maxGuestAMount, ICollection<InRoomFacility> commudities)
     {
       Name = typeName;
