b8548e871422a48539357fff41ab22227940eeb5
SUMETH K <sumeth_dev@live.com>
2022-08-29 16:48:06 +0700

    Add class InRoomFacility

---
+++ HotelApp/Models/InRoomFacility.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelApp.Models
+{
+  internal class InRoomFacility : HotelFacility
+  {
+    public Guid RoomId { get; }
+
+    public InRoomFacility(Guid hotelId, string facilityName, int facilityAmount) : base(hotelId, facilityName, facilityAmount)
+    {
+    }
+
+    public InRoomFacility(string facilityName, int facilityAmount, Guid hotelId, Guid roomId) : this(hotelId, facilityName, facilityAmount)
+    {
+      RoomId = roomId;
+    }
+  }
+}
