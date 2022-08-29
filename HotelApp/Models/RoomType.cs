83de52c177fb372ebad81aa1dada464dd17e6487
SUMETH K <sumeth_dev@live.com>
2022-08-29 16:52:45 +0700

    Add class RoomType

---
+++ HotelApp/Models/RoomType.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelApp.Models
+{
+  internal class RoomType
+  {
+    public string Name { get; }
+    public int MaxGuestAMount { get; }
+
+    public ICollection<InRoomFacility> Commudities { get; private set; }
+    public RoomType(string typeName, int maxGuestAMount, ICollection<InRoomFacility> commudities)
+    {
+      Name = typeName;
+      MaxGuestAMount = maxGuestAMount;
+      Commudities = new HashSet<InRoomFacility>(commudities);
+    }
+  }
+}
