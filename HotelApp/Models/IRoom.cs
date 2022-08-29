885e2d8c2222ef1a4db4b1ebee073d7947956669
SUMETH K <sumeth_dev@live.com>
2022-08-29 17:09:37 +0700

    Add interface IRoom

---
+++ HotelApp/Models/IRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelApp.Models
+{
+  internal interface IRoom
+  {
+    Guid HotelId { get; }
+    int FloorNumber { get; }
+    Guid RoomId { get; }
+    string RoomName { get; }
+    RoomStatus RoomStatus { get; }
+    RoomType RoomType { get; }
+    ICollection<Guest> Guests { get; }
+    ICollection<KeycardAllowCheckinRoom> KeycardAllowAccessRooms { get; }
+  }
+}
