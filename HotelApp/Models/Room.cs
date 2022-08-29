48be25e34adccb65130f1231dbb5a8583f85c30a
SUMETH K <sumeth_dev@live.com>
2022-08-29 17:09:48 +0700

    Add class room

---
+++ HotelApp/Models/Room.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Models
+{
+  internal abstract class Room : IRoom
+  {
+    private readonly Guid _hotelId;
+    private readonly int _floorNumber;
+    private readonly Guid _roomId;
+    private readonly string _roomName;
+    private readonly RoomType _roomType;
+    protected RoomStatus _roomStatus;
+
+    public Room(Guid hotelId, int floorNumber, string roomName, RoomType roomType)
+    {
+      _roomId = Guid.NewGuid();
+      _hotelId = hotelId;
+      _floorNumber = floorNumber;
+      _roomName = roomName;
+      _roomType = roomType;
+    }
+
+    public Guid HotelId => _hotelId;
+
+    public int FloorNumber => _floorNumber;
+
+    public Guid RoomId => _roomId;
+
+    public string RoomName => _roomName;
+
+    public RoomStatus RoomStatus => _roomStatus;
+
+    public RoomType RoomType => _roomType;
+
+    public virtual ICollection<Guest> Guests => new HashSet<Guest>();
+
+    public virtual ICollection<KeycardAllowCheckinRoom> KeycardAllowAccessRooms => new HashSet<KeycardAllowCheckinRoom>();
+  }
+}
